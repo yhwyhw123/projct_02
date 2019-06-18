d1ef5ee21241fbd506dfae1b98038a164641c23d
yhwyhw123 <1306380241@qq.com>
2019-06-18 11:05:26 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+import appium
+from selenium import webdriver
+
+class TestLogin:
+    def test_login1(self):
+        print("login1")
+        assert 1
+    def test_login2(self):
+        print("login2")
+        assert 1
+    def test_login3(self):
+        print("login3")
+        assert 1
